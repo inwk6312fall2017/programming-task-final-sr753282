ea73eddce7fa13bb98c90b1023c870390d63419b
sr753282 <sarveshgupta@dal.ca>
2017-10-20 12:04:28 -0700

    updated

--- task1.py
+++ task1.py
@@ -25,7 +25,7 @@ for i1 in stringsplit:
     e1 = max(d1)
 for j1 in stringsplit:
     if len(j1) == e1:
-        print("The longest word is", j1)
+        print("The longest word in book2 is",j1)
 # reading the third file and finding the longest word
 
 d2 = open('Book3.txt','r')
@@ -39,4 +39,12 @@ for i2 in stringsplit:
     e2 = max(d2)
 for j2 in stringsplit:
     if len(j2) == e2:
-        print("The longest word is", j2)
+        print("The longest word in Book3 is",j2)
+
+# comparing the longest words from book1,book2 and book3 now
+if (j>j1>j2):
+    print("the longest word  is",j)
+elif (j1>j2):
+    print("the longest word is",j1)
+else:
+         print("the longest word is ",j2)
