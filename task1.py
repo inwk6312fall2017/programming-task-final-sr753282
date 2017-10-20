c4e9f850751e9df8e92b3e3c5f1dbcf5ea788d1a
sr753282 <sarveshgupta@dal.ca>
2017-10-20 10:27:19 -0700

    task 1 reading files and finding the longest word

---
+++ task1.py
@@ -0,0 +1,42 @@
+
+#reading the first file and finding the longest word
+d = open('Book1.txt','r')
+l = d.readlines()
+string = l[0]
+stringsplit = string.split()
+
+d = []
+for i in stringsplit:
+    d.append(len(i))
+    e = max(d)
+for j in stringsplit:
+    if len(j) == e:
+        print("The longest word in Book1 is", j)
+#reading the second file and finding the longest word
+
+d1 = open('Book2.txt','r')
+l1 = d1.readlines()
+string = l1[0]
+stringsplit = string.split()
+
+d1 = []
+for i1 in stringsplit:
+    d1.append(len(i1))
+    e1 = max(d1)
+for j1 in stringsplit:
+    if len(j1) == e1:
+        print("The longest word is", j1)
+# reading the third file and finding the longest word
+
+d2 = open('Book3.txt','r')
+l2 = d2.readlines()
+string = l2[0]
+stringsplit = string.split()
+
+d2 = []
+for i2 in stringsplit:
+    d2.append(len(i2))
+    e2 = max(d2)
+for j2 in stringsplit:
+    if len(j2) == e2:
+        print("The longest word is", j2)
